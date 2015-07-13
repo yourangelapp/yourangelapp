8d39db5741cbb5d1c11473dd3fb98f9dee778f1f
daschy <miloertola@gmail.com>
2015-07-13 18:03:55 +0200

    fixf

--- js/custom.js
+++ js/custom.js
@@ -8,8 +8,9 @@ jQuery(window).load(function() {
         // will fade out the whole DIV that covers the website.
 	jQuery(".preloader").delay(1000).fadeOut("slow");
 
-    $.get("http://ipinfo.io", function(response) {
-        if("MX"===response.country){
+    $.get("https://freegeoip.net/json/", function(response) {
+        // console.log(response);
+        if("MX"===response.country_code){
 
             $('.number').html('5566751005');
             $('.number-alt').parent().parent().hide();
